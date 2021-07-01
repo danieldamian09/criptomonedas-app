fb430911910f9a0d1ff6cbf88b082cadf9fc23e2
humberto <humbertorivero9@gmail.com>
2021-07-01 14:34:34 -0300

    Creando componente principal y Componente de Imagen

---
+++ criptomonedas/src/App.js
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled'
+import imagen from './cryptomonedas.png'
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+function App() {
+  return (
+    <Contenedor>
+      <div>
+        <Imagen 
+          src={imagen}
+          alt= "imagen Criptomonedas";
+        />
+      </div>
+
+      <div>
+
+      </div>
+    </Contenedor>
+  );
+}
+
+export default App;
